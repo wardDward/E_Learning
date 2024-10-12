80ec457510e37f343e3f57e3aec61b433541bbab
Edward <edward.taligatos03@gmail.com>
2024-10-13 01:32:20 +0800

    basic routing setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navyBlue: "#2C3E50",
+        customTeal: "#1ABC9C",
+        coral: "#E74C3C",
+        ligthGray: "#ECF0F1",
+        charcoal: "#34495E",
+        darkGray: "#7F8C8D",
+        customGreen: "#27AE60",
+      },
+    },
   },
   plugins: [],
-}
+};
